596071589a2854a8755549bd34af2e48a825023e
Anton Kulakov <anton.kulakoff@ya.ru>
2024-09-22 16:04:22 +0300

    feat: added syslog message parser

--- internal/syslog_custom/syslog.go
+++ internal/syslog_custom/syslog.go
@@ -2,10 +2,12 @@ package syslog_custom
 
 import (
 	"fmt"
-	"github.com/leodido/go-syslog/rfc5424"
 	"log"
 	"log/slog"
 	"net"
+	"regexp"
+	"strings"
+	"time"
 )
 
 type SyslogServer struct {
@@ -13,6 +15,18 @@ type SyslogServer struct {
 	unit    string // panel type: beward, qtech, ...
 	logger  *slog.Logger
 	handler MessageHandler
+}
+
+type SyslogMessage struct {
+	Format         string    `json:"format"`
+	Priority       int       `json:"priority"`
+	Version        int       `json:"version"`
+	Timestamp      time.Time `json:"timestamp"`
+	HostName       string    `json:"hostname"`
+	AppName        string    `json:"appName"`
+	ProcID         string    `json:"procId"`
+	StructuredData string    `json:"structuredData"`
+	Message        string    `json:"message"`
 }
 
 type MessageHandler interface {
@@ -36,9 +50,8 @@ func (s *SyslogServer) Start() {
 	s.logger.Info("Syslog server running", "unit", s.unit, "port", s.port)
 
 	buffer := make([]byte, 1024)
-	parser := rfc5424.NewParser()
 	for {
-		n, _, err := conn.ReadFromUDP(buffer)
+		n, srcIP, err := conn.ReadFromUDP(buffer)
 		if err != nil {
 			log.Printf("Error reading from UDP: %v", err)
 			continue
@@ -47,14 +60,12 @@ func (s *SyslogServer) Start() {
 		message := string(buffer[:n])
 		log.Printf("HW: %s | Raw Message: %s", s.unit, message)
 
-		// Парсинг syslog сообщения
-		syslogMessage, err := parser.Parse(buffer[:n], nil)
+		parsedMessage, err := s.ParseMessage(message)
 		if err != nil {
-			log.Printf("Error parsing syslog message: %v", err)
-			continue
+			s.logger.Warn("Error parsing message", "error", err)
 		}
 
-		log.Printf("Parsed Syslog Message: %+v", syslogMessage.Message())
+		slog.Info("process-log", "src-ip", srcIP, "msg-ip", parsedMessage.HostName, "msg", parsedMessage.Message)
 
 		//s.handler.HandleMessage(srcIP, message)
 	}
@@ -67,4 +78,33 @@ func New(port int, unit string, logger *slog.Logger, handler MessageHandler) *Sy
 		logger:  logger,
 		handler: handler,
 	}
+}
+
+func (s *SyslogServer) ParseMessage(rawMessage string) (*SyslogMessage, error) {
+	rfc5424Regex := regexp.MustCompile(`^<(?P<priority>\d|\d{2}|1[1-8]\d|19[01])>(?P<version>\d{1,2})\s(?P<timestamp>-|(?P<fullyear>[12]\d{3})-(?P<month>0\d|[1][012])-(?P<mday>[012]\d|3[01])T(?P<hour>[01]\d|2[0-4]):(?P<minute>[0-5]\d):(?P<second>[0-5]\d|60)(?:\.(?P<secfrac>\d{1,6}))?(?P<numoffset>Z|[+-]\d{2}:\d{2}))\s(?P<hostname>[\S]{1,255})\s(?P<appname>[\S]{1,48})\s(?P<procid>[\S]{1,128})\s(?P<msgid>[\S]{1,32})\s(?P<structureddata>-|(?:\[.+?\]))(?:\s(?P<message>.+))?$`)
+
+	rawMessage = strings.TrimSpace(rawMessage)
+
+	if matches := rfc5424Regex.FindStringSubmatch(rawMessage); matches != nil {
+		regexpGroupNames := rfc5424Regex.SubexpNames()
+
+		// TODO : refactor map. test memory usage
+		matchMap := make(map[string]string)
+		for i, match := range matches {
+			if regexpGroupNames[i] != "" {
+				matchMap[regexpGroupNames[i]] = match
+			}
+		}
+
+		hostname := matchMap["hostname"]
+		message := matchMap["message"]
+
+		return &SyslogMessage{
+			HostName: hostname,
+			Message:  message,
+		}, nil
+
+	}
+
+	return nil, fmt.Errorf("ParseMessage, unsupported message format: %s", rawMessage)
 }
