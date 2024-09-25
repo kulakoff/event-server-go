0dd7ae90d9f573f2dca48396405f73a404849625
Anton Kulakov <anton.kulakoff@ya.ru>
2024-09-25 22:46:29 +0300

    add storage packege

--- internal/storage/remote_storage.go
+++ internal/storage/remote_storage.go
@@ -1,6 +1,8 @@
 package storage
 
 import (
+	"context"
+	"fmt"
 	"github.com/ClickHouse/clickhouse-go/v2"
 	"log/slog"
 )
@@ -8,4 +10,25 @@ import (
 type ClikhouseHandler struct {
 	logger     *slog.Logger
 	clickhouse clickhouse.Conn
+}
+
+func New(logger *slog.Logger, dsn string) (*ClikhouseHandler, error) {
+	conn, err := clickhouse.Open(&clickhouse.Options{
+		Addr: []string{dsn},
+		Auth: clickhouse.Auth{
+			Username: "default",
+			Password: "qqq",
+			Database: "default",
+		},
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("failed connect to clickhouse dsn %s: %w ", dsn, err)
+	}
+
+	if err := conn.Ping(context.Background()); err != nil {
+		return nil, fmt.Errorf("failed ping clickhouse dsn %s: %w ", dsn, err)
+	}
+
+	return &ClikhouseHandler{logger: logger, clickhouse: conn}, nil
 }
