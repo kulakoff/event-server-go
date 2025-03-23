c748767a1aa2962915e76b38add3f5e2c8187d69
Anton Kulakov <anton.kulakoff@ya.ru>
2025-03-23 15:20:14 +0300

    refactor: updated psql storage

--- internal/storage/psql.go
+++ internal/storage/psql.go
@@ -11,42 +11,51 @@ import (
 
 type PSQLStorage struct {
 	logger *slog.Logger
-	db     *pgxpool.Pool
+	DB     *pgxpool.Pool
 }
 
 func NewPSQLStorage(logger *slog.Logger, postgresConfig *config.PostgresConfig) (*PSQLStorage, error) {
+	// Format connection str
 	connStr := formatPostgresURL(postgresConfig)
+
+	// Creat connection pool
 	psqlConf, err := pgxpool.ParseConfig(connStr)
 	if err != nil {
 		return nil, fmt.Errorf("unable to parse DSN: %w", err)
 	}
+	// configure connection pool
 	psqlConf.MaxConns = 10 // max connect
 	psqlConf.ConnConfig.ConnectTimeout = 5 * time.Second
 
+	// Connect to db
 	db, err := pgxpool.NewWithConfig(context.Background(), psqlConf)
 	if err != nil {
 		return nil, fmt.Errorf("unable to connect to database: %w", err)
 	}
 
-	// check connection
+	// Check connection
 	if err := db.Ping(context.Background()); err != nil {
 		logger.Error("Unable to ping database", "error", err)
 		return nil, fmt.Errorf("unable to ping database: %w", err)
 	}
 
 	logger.Info("Successfully connected to PostgreSQL")
+
 	return &PSQLStorage{
 		logger: logger,
-		db:     db}, nil
+		DB:     db,
+	}, nil
 }
 
+// Close db connection
 func (s *PSQLStorage) Close() {
-	if s.db != nil {
-		s.db.Close()
+	if s.DB != nil {
+		s.DB.Close()
 		s.logger.Info("Success closed connection to PostgreSQL")
 	}
 }
 
+// formatPostgresURL config to connection URI
 func formatPostgresURL(cfg *config.PostgresConfig) string {
 	//urlExample := "postgres://username:password@localhost:5432/database_name"
 	//return fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=%s",
